a97b114108c10e26621ae88cd5ad57db69066fca
cheerfyt <tate_fan@163.com>
2019-08-01 18:15:18 +0800

    clear all case

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+	# katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/search_worktile_from_baidu"
 docker run -t --rm \
 	-v "$(pwd)":/katalon/katalon/source katalonstudio/katalon \
-	katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuitePath="Test Suites/search_worktile_from_baidu"
+	katalon-execute.sh -browserType="Chrome" -retry=0 -statusDelay=15 -testSuiteCollectionPath="Test Suites/test_suites_collections"
