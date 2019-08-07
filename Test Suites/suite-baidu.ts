1aa2ff74506ca9fdeeeb626f528c58db9d9944fc
cheerfyt <tate_fan@163.com>
2019-08-07 14:11:12 +0800

    feat(autotestIntegrations): add case1

---
+++ Test Suites/suite-baidu.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>suite-baidu</description>
+   <name>suite-baidu</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>31103c45-98ca-48ae-abf3-242272e863c0</testSuiteGuid>
+   <testCaseLink>
+      <guid>c3271433-f15d-4fa8-a148-26bbf6542b9d</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/baidu</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
