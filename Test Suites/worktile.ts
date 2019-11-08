a0e68ab5bfd3eda2f6eca23ce08ab3c61aac60aa
cheerfyt <tate_fan@163.com>
2019-11-08 16:21:12 +0800

    use worktile case

---
+++ Test Suites/worktile.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>worktile</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>99b9be4f-5301-4447-ac7f-cc4ff18e8956</testSuiteGuid>
+   <testCaseLink>
+      <guid>6858c4b5-60fa-41eb-884b-39503dd386e0</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/worktile</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
